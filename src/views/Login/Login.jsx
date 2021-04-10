b23e590b96fdd98f75b0fb1e65968e592efa3935
jiang qiang <jiangtingqiang@gmail.com>
2021-04-10 19:48:41 +0800

    [feature] 添加后台登录

--- src/views/Login/Login.jsx
+++ src/views/Login/Login.jsx
@@ -1,8 +1,8 @@
 import React, { Component } from 'react'
 import { Layout, Input, Icon, Form, Button, Divider, message, notification } from 'antd'
 import { withRouter } from 'react-router-dom'
-// import axios from '@/api'
-// import { API } from '@/api/config'
+import axios from '@/api'
+import { API } from '@/api/config'
 import '@/style/view-style/login.scss'
 
 class Login extends Component {
@@ -16,51 +16,51 @@ class Login extends Component {
         })
     }
 
+    handleLoginSuccess = (values) => {
+        // 这里可以做权限校验 模拟接口返回用户权限标识
+        switch (values.username) {
+            case 'admin':
+                values.auth = 0
+                break
+            default:
+                values.auth = 0
+        }
+
+        localStorage.setItem('user', JSON.stringify(values))
+        this.enterLoading()
+        message.success('登录成功!')
+        this.props.history.push('/')
+    }
+
     handleSubmit = e => {
         e.preventDefault()
         this.props.form.validateFields((err, values) => {
             if (!err) {
                 let { username, password } = values
-                //axios
-                //     .post(`${API}/login`, { username, password })
-                //     .then(res => {
-                //         if (res.data.code === 0) {
-                //             localStorage.setItem('user', JSON.stringify(res.data.data.user))
-                //             localStorage.setItem('token', res.data.data.token)
-                //             this.props.history.push('/')
-                //             message.success('登录成功!')
-                //         } else {
-                //             // 这里处理一些错误信息
-                //         }
-                //     })
-                //     .catch(err => {})
-
-                // 这里可以做权限校验 模拟接口返回用户权限标识
-                switch (values.username) {
-                    case 'admin':
-                        values.auth = 0
-                        break
-                    default:
-                        values.auth = 1
-                }
-
-                localStorage.setItem('user', JSON.stringify(values))
-                this.enterLoading()
-                this.timer = setTimeout(() => {
-                    message.success('登录成功!')
-                    this.props.history.push('/')
-                }, 2000)
+                var request = {
+                    "phone":username,
+                    "password": password
+                };
+                axios
+                    .post(`${API}/manage/user/login`, JSON.stringify(request))
+                    .then(res => {
+                        if (res.data.statusCode === '200' && res.data.resultCode === '200') {
+                            //localStorage.setItem('user', JSON.stringify(res.data.data.user))
+                            localStorage.setItem('token', res.data.result)
+                            this.props.history.push('/')
+                            this.handleLoginSuccess(values)
+                        } else {
+                            // 这里处理一些错误信息
+                            message.error('登录失败：' + res.data.msg)
+                            return
+                        }
+                    })
+                    .catch(err => {})
             }
         })
     }
 
-    componentDidMount() {
-        notification.open({
-            message: '欢迎使用后台管理平台',
-            duration: null,
-            description: '账号 admin(管理员) 其他(游客) 密码随意'
-        })
-    }
+    componentDidMount() {}
 
     componentWillUnmount() {
         notification.destroy()
