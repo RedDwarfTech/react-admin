d5c5381a95fc5f762e60e4f6aa6abfc4c6c7ff99
jiang qiang <jiangtingqiang@gmail.com>
2021-04-11 21:01:46 +0800

    [feature] routes的props传递调整，可以成功传递所有props

---
+++ src/views/App/Cruise/User/User.jsx
@@ -0,0 +1,135 @@
+import React, { Component } from 'react'
+import CustomBreadcrumb from '@/components/CustomBreadcrumb'
+import { Layout, Divider, Row, Col, Tag, Table, Button, Anchor, message, notification, Form } from 'antd'
+import '@/style/view-style/table.scss'
+import { withRouter } from 'react-router-dom'
+import { getUserList } from '../../../../service/cruise/UserService';
+
+const { Link } = Anchor
+
+const columns = [
+    {
+        title: 'ID',
+        dataIndex: 'id',
+        key: 'id',
+    },
+    {
+        title: '订阅状态',
+        dataIndex: 'subStatus',
+        key: 'subStatus',
+        render: text => (
+            text === 1?<span>{'正常'}</span>:<span>{'停止订阅'}</span>
+        )
+    },
+    {
+        title: '操作',
+        key: 'action',
+        render: (text, record) => (
+            <span>
+                <Button type='link'>详情</Button>
+                <Divider type='vertical' />
+                <Button type='link'>删除</Button>
+            </span>
+        )
+    }
+]
+class User extends Component {
+    state = {
+        loading: false,
+        pageNum: 1,
+        pageSize: 10,
+    }
+
+    constructor(props) {
+        super(props);
+    }
+
+    enterLoading = () => {
+        this.setState({
+            loading: true
+        })
+    }
+
+    onPageChange = current => {
+        this.setState({
+            pageNum: current,
+        });
+        let request = {
+            pageSize: this.state.pageSize,
+            pageNum: current
+        };
+        getUserList(request);
+    };
+    changePageSize(pageSize, current) {
+        // 将当前改变的每页条数存到state中
+        this.setState({
+            pageSize: pageSize,
+        });
+        let request = {
+            pageSize: pageSize,
+            pageNum: this.state.pageNum
+        };
+        getUserList(request);
+    }
+    
+    componentDidMount() {
+        let request = {
+            pageSize: this.state.pageSize,
+            pageNum: this.state.pageNum
+        };
+        getUserList(request);
+    }
+
+    componentWillUnmount() {
+        notification.destroy()
+        this.timer && clearTimeout(this.timer)
+    }
+
+    render() {
+        const { getFieldDecorator } = this.props.form
+        let data = this.props.user.user.list;
+        let users = this.props.user.user;
+
+        if((data && Object.keys(data).length === 0)|| data == undefined){
+            return (<div></div>);
+        }
+
+        let total = parseInt(users.pagination.total);
+
+        const paginationProps = {
+            showSizeChanger: true,
+            showQuickJumper: true,
+            pageSize:users.pagination.pageSize,
+            pageSizeOptions:['10','20','30'],
+            showTotal: () => `共${total}条`,
+            current: users.pagination.pageNum,
+            total: total,
+            onShowSizeChange: (current, pageSize) => this.changePageSize(pageSize,current),
+            onChange: current => this.onPageChange(current),
+        };
+
+        return (
+            <Layout className='animated fadeIn'>
+                <div>
+                    <CustomBreadcrumb arr={['应用', 'Cruise', '订阅源']}></CustomBreadcrumb>
+                </div>
+
+                <Row>
+                    <Col>
+                        <div className='base-style'>
+                            <h3 id='basic'>订阅源管理</h3>
+                            <Divider />
+                            <Table 
+                            columns={columns} 
+                            dataSource={data} 
+                            pagination={paginationProps}
+                            rowKey = 'id' />
+                        </div>
+                    </Col>
+                </Row>
+            </Layout>
+        )
+    }
+}
+
+export default withRouter(Form.create()(User))
