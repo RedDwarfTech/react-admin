9d8a65378762b6f4fef9ffc5ddba61b5ceeb1335
jiang qiang <jiangtingqiang@gmail.com>
2021-04-11 17:29:16 +0800

    [feature] 新增翻页方案

--- src/views/App/Cruise/Channel/Channel.jsx
+++ src/views/App/Cruise/Channel/Channel.jsx
@@ -63,7 +63,9 @@ const columns = [
 ]
 class Channel extends Component {
     state = {
-        loading: false
+        loading: false,
+        pageNum: 1,
+        pageSize: 10,
     }
 
     constructor(props) {
@@ -76,6 +78,28 @@ class Channel extends Component {
         })
     }
 
+    onPageChange = current => {
+        this.setState({
+            pageNum: current,
+        });
+        let request = {
+            pageSize: this.state.pageSize,
+            pageNum: current
+        };
+        getChannelList(request);
+    };
+    changePageSize(pageSize, current) {
+        // 将当前改变的每页条数存到state中
+        this.setState({
+            pageSize: pageSize,
+        });
+        let request = {
+            pageSize: pageSize,
+            pageNum: this.state.pageNum
+        };
+        getChannelList(request);
+    }
+    
     componentDidMount() {
         getChannelList('');
     }
@@ -88,10 +112,23 @@ class Channel extends Component {
     render() {
         const { getFieldDecorator } = this.props.form
         let data = this.props.channel.channel.list;
+        let channel = this .props.channel.channel;
 
         if((data && Object.keys(data).length === 0)|| data == undefined){
             return (<div></div>);
         }
+
+        const paginationProps = {
+            showSizeChanger: true,
+            showQuickJumper: true,
+            pageSize:channel.pagination.pageSize,
+            pageSizeOptions:['10','20','30'],
+            // showTotal: () => `共${total}条`,
+            current: channel.pagination.pageNum,
+            total: channel.pagination.total,
+            onShowSizeChange: (current, pageSize) => this.changePageSize(pageSize,current),
+            onChange: current => this.onPageChange(current),
+        };
 
         return (
             <Layout className='animated fadeIn'>
@@ -104,7 +141,7 @@ class Channel extends Component {
                         <div className='base-style'>
                             <h3 id='basic'>订阅源管理</h3>
                             <Divider />
-                            <Table columns={columns} dataSource={data} />
+                            <Table columns={columns} dataSource={data} pagination={paginationProps} />
                         </div>
                     </Col>
                 </Row>
