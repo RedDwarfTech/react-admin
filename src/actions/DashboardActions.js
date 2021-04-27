e6572921cb08941aa1dc05926032342a3537cdf4
jiangxiaoqiang <jiangtingqiang@gmail.com>
2021-04-28 00:09:36 +0800

    [feature] 新增文章抓取数量统计显示

--- src/actions/DashboardActions.js
+++ src/actions/DashboardActions.js
@@ -1,9 +1,16 @@
 /**
  * Created by dolphin on 15/7/2017.
  */
- export function getDashboardAction(payload) {
+export function getDashboardAction(payload) {
     return {
-        type: "GET_DASHBOARD_DATA",
+        type: 'GET_DASHBOARD_DATA',
         payload: payload
-    };
+    }
+}
+
+export function getTrendAction(payload) {
+    return {
+        type: 'GET_TREND_DATA',
+        payload: payload
+    }
 }
--- src/views/Index/line.jsx
+++ src/views/Index/line.jsx
@@ -4,59 +4,67 @@ import 'echarts/lib/chart/line'
 import 'echarts/lib/component/tooltip'
 import 'echarts/lib/component/title'
 import 'echarts/lib/component/legend'
+import { fetchTrend } from '../../service/cruise/DashboardService'
 
 class Line extends Component {
+    state = {
+        trend: {}
+    }
+
     componentDidMount() {
-        let myChart = echarts.init(document.getElementById('line'))
-        myChart.setOption({
-            tooltip: {
-                trigger: 'axis'
-            },
-            legend: {
-                data: ['邮件营销', '联盟广告', '视频广告']
-            },
-            grid: {
-                left: '3%',
-                right: '4%',
-                bottom: '3%',
-                containLabel: true
-            },
-            toolbox: {
-                feature: {
-                    saveAsImage: {}
-                }
-            },
-            xAxis: {
-                type: 'category',
-                boundaryGap: false,
-                data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
-            },
-            yAxis: {
-                type: 'value'
-            },
-            series: [
-                {
-                    name: '邮件营销',
-                    type: 'line',
-                    data: [120, 132, 101, 134, 90, 230, 210]
-                },
-                {
-                    name: '联盟广告',
-                    type: 'line',
-                    data: [220, 182, 191, 234, 290, 330, 310]
-                },
-                {
-                    name: '视频广告',
-                    type: 'line',
-                    data: [150, 232, 201, 154, 190, 330, 410]
-                }
-            ]
-        })
-        window.addEventListener('resize', function() {
-            myChart.resize()
-        })
+        fetchTrend({})
     }
     render() {
+        let data = this.props.trend
+
+        if ((data && Object.keys(data).length === 0) || data === undefined) {
+        } else {
+            let myChart = echarts.init(document.getElementById('line'))
+            myChart.setOption({
+                tooltip: {
+                    trigger: 'axis'
+                },
+                legend: {
+                    data: ['文章', '频道']
+                },
+                grid: {
+                    left: '3%',
+                    right: '4%',
+                    bottom: '3%',
+                    containLabel: true
+                },
+                toolbox: {
+                    feature: {
+                        saveAsImage: {}
+                    }
+                },
+                xAxis: {
+                    type: 'category',
+                    boundaryGap: false,
+                    data: this.props.trend.length > 0 ? this.props.trend.map(item => item.statisticTime) : ['ddd']
+                },
+                yAxis: {
+                    type: 'value'
+                },
+                series: [
+                    {
+                        name: '文章',
+                        type: 'line',
+                        data: this.props.trend.length > 0 ? this.props.trend.map(item => item.increNum) : [1]
+                    },
+                    {
+                        name: '频道',
+                        type: 'line',
+                        data: this.props.trend.length > 0 ? this.props.trend.map(item => item.increNum) : [1]
+                    }
+                ]
+            })
+
+            window.addEventListener('resize', function() {
+                myChart.resize()
+            })
+        }
+
         return <div id='line' style={{ height: 300 }}></div>
     }
 }
