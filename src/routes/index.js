da3a98b36bfe94d40c741f936c65934726e69d3a
jiangxiaoqiang <jiangtingqiang@gmail.com>
2021-06-20 05:36:15 -0400

    [feature] 新增密码修改界面

--- src/routes/index.js
+++ src/routes/index.js
@@ -42,23 +42,13 @@ const ArticleDetail = loadable(() =>
     import(/* webpackChunkName: 'about' */ '@/views/App/Cruise/Article/ArticleDetail/ArticleDetail')
 )
 
+const Password = loadable(() => import(/* webpackChunkName: 'about' */ '@/views/Profile/Password'))
+
 const routes = [
     { path: '/index', exact: true, name: 'Index', component: Index, auth: [1] },
     //{ path: '/public/button', exact: false, name: '按钮', component: ButtonView, auth: [1] },
     //{ path: '/public/icon', exact: false, name: '图标', component: IconView, auth: [1] },
     //{ path: '/nav/dropdown', exact: false, name: '下拉菜单', component: DropdownView },
-    //{ path: '/nav/menu', exact: false, name: '下拉菜单', component: MenuView },
-    //{ path: '/nav/steps', exact: false, name: '步骤条', component: StepView },
-    //{ path: '/form/base-form', exact: false, name: '表单', component: FormBaseView },
-    //{ path: '/form/step-form', exact: false, name: '表单', component: FormStepView },
-    //{ path: '/show/table', exact: false, name: '表格', component: TableView },
-    //{ path: '/show/collapse', exact: false, name: '折叠面板', component: CollapseView },
-    //{ path: '/show/tree', exact: false, name: '树形控件', component: TreeView },
-    //{ path: '/show/tabs', exact: false, name: '标签页', component: TabsView },
-    //{ path: '/others/progress', exact: false, name: '进度条', component: ProgressView, auth: [1] },
-    //{ path: '/others/animation', exact: false, name: '动画', component: AnimationView, auth: [1] },
-    //{ path: '/others/editor', exact: false, name: '富文本', component: EditorView, auth: [1] },
-    //{ path: '/others/upload', exact: false, name: '上传', component: UploadView, auth: [1] },
     {
         path: '/app/overview/app',
         exact: false,
@@ -94,6 +84,12 @@ const routes = [
         exact: false,
         name: '文章详情',
         component: ArticleDetail
+    },
+    {
+        path: '/profile/password',
+        exact: false,
+        name: '修改密码',
+        component: Password
     },
     { path: '/about', exact: false, name: '关于', component: About, auth: [1] }
 ]
