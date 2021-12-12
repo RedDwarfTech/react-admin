628041a0ae001e599843a786ec6b4786879694b0
xiaoqiang jiang <xiaoqiangjiang@xiaoqiangs-MacBook-Pro.local>
2021-12-12 23:56:30 +0800

    [feature] tweak

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,8 +1,8 @@
-const { createProxyMiddleware } = require('http-proxy-middleware')
+const proxy = require('http-proxy-middleware')
 
 module.exports = function(app) {
     app.use(
-        createProxyMiddleware('//manage/?$/', {
+        proxy('//manage/?$/', {
             target: 'https://admin.poemhub.top/',
             changeOrigin: true
         })
