a7039eae663baa5b14396a620759195a93ac11d3
jiang qiang <jiangtingqiang@gmail.com>
2021-12-24 18:08:03 +0800

    [feature] remove home unused graph

--- src/views/Index/Index.jsx
+++ src/views/Index/Index.jsx
@@ -40,41 +40,10 @@ class Index extends Component {
                         </div>
                     </Col>
                 </Row>
-                <Row>
-                    <Col>
-                        <div className='base-style'>
-                            <div className='bar-header'>
-                                <div>图形全屏展示</div>
-                                <FullscreenOutlined
-                                    type='fullscreen'
-                                    style={{ cursor: 'pointer' }}
-                                    onClick={this.fullToggle}
-                                />
-                            </div>
-                            <Divider />
-                            <BarEcharts />
-                        </div>
-                    </Col>
-                </Row>
                 <Row gutter={8}>
                     <Col span={12}>
                         <div className='base-style'>
                             <LineEcharts trend={this.props.dashboard.trend} />
-                        </div>
-                    </Col>
-                    <Col span={12}>
-                        <div className='base-style'>
-                            <PieEcharts />
-                        </div>
-                    </Col>
-                    <Col span={12}>
-                        <div className='base-style'>
-                            <ScatterEcharts />
-                        </div>
-                    </Col>
-                    <Col span={12}>
-                        <div className='base-style'>
-                            <PictorialBarEcharts />
                         </div>
                     </Col>
                 </Row>
