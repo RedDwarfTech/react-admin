d8164c4df6552f30c7cf4ffe8c5a4dc989cb194c
JiangXiaoqiang <jiangtingqiang@gmail.com>
2022-01-17 23:58:12 +0800

    tweak

--- src/service/global/AppService.js
+++ src/service/global/AppService.js
@@ -1,5 +1,4 @@
-
-import { getAppListAction } from '@/actions/AppActions'
+import { getAppListAction, addAppAction } from '@/actions/global/AppActions'
 import { requestWithAction } from '@/api/XHRClient'
 
 export function getAppList(request) {
@@ -7,7 +6,15 @@ export function getAppList(request) {
         method: 'post',
         url: `/manage/app/v1/page`,
         data: request
-    };
-    return requestWithAction(config, getAppListAction);
+    }
+    return requestWithAction(config, getAppListAction)
 }
 
+export function addApp(request) {
+    const config = {
+        method: 'post',
+        url: `/manage/app/v1/add`,
+        data: request
+    }
+    return requestWithAction(config, addAppAction)
+}
