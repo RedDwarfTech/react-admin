1898c124dcf6ea10e97c24506cd5c2e346a455fe
JiangXiaoqiang <jiangtingqiang@gmail.com>
2022-02-20 21:57:14 +0800

    [tweak] remove build-bak

--- build-bak/static/js/500.716356bb.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpreact-admin"]=this["webpackJsonpreact-admin"]||[]).push([[6],{538:function(t,e,a){t.exports=a.p+"static/media/500.4afd5a57.png"},669:function(t,e,a){"use strict";a.r(e);var n=a(0),c=a.n(n),i=a(538),r=a.n(i),s=function(){return c.a.createElement("div",{style:{textAlign:"center"}},c.a.createElement("img",{src:r.a,alt:""}))};e.default=s}}]);
-//# sourceMappingURL=500.716356bb.chunk.js.map
