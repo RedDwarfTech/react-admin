b9e993099898287681ee4c310dae26fde1b728bc
JiangXiaoqiang <jiangtingqiang@gmail.com>
2022-02-22 00:01:24 +0800

    [build] add css compress config

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Button, DatePicker, version } from 'antd'
 import 'antd/dist/antd.css'
-import './index.css'
 import { Provider } from 'react-redux'
 import store from './store'
 import App from './App'
