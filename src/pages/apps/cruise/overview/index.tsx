63eb4bf209b5692da4ad34c6732681a58d248cc4
JiangXiaoqiang <jiangtingqiang@gmail.com>
2022-07-25 10:38:43 +0800

    feat: add article top 10 demo

--- src/pages/apps/cruise/overview/index.tsx
+++ src/pages/apps/cruise/overview/index.tsx
@@ -1,24 +1,15 @@
-import { Card, Layout, Row, Col } from 'antd';
-import React, { useState } from 'react';
+import { Card, Row, Col, Button, Space, Tag } from 'antd';
+import React, { useRef, useState } from 'react';
 import { ITrendPageProps, ITrendState } from 'umi';
 import { connect, Loading, Dispatch } from 'umi'
 import ReactECharts from 'echarts-for-react';
 import dayjs from 'dayjs';
-import Icon from '@ant-design/icons';
+import { ProList } from '@ant-design/pro-components';
 //import '@/style/view-style/index.scss'
 import styles from './index.less'
+import { ProDescriptionsActionType } from '@ant-design/pro-descriptions';
 
 const TableList: React.FC<ITrendPageProps> = ({ trends, dispatch, loading }) => {
-
-  const [recommendStatus, hanleUpdateRecommendStatus] = useState<{
-    editorPick: number | null,
-    minimalReputation: number | null,
-    isTag: number|null
-  }>({
-    editorPick: null,
-    minimalReputation: 0,
-    isTag: 0
-  });
 
   const handleRequest = (start: number, end: number): void => {
     dispatch({
@@ -36,30 +27,95 @@ const TableList: React.FC<ITrendPageProps> = ({ trends, dispatch, loading }) => 
     handleRequest(monthStartMilli, monthEndMilli);
   }, []);
 
-  const onRadioClick = (e: any) => {
-    hanleUpdateRecommendStatus({
-      editorPick: Number(e.target.value) === 1 ? 1 : null,
-      minimalReputation: Number(e.target.value) === 0 ? 1 : 0,
-      isTag: Number(e.target.value) === 2? 0:null,
-    });
-    let editorPick = Number(e.target.value) === 1 ? 1 : 0;
-    let minimalReputation = Number(e.target.value) === 0 ? 1 : 0;
-    let isTag = Number(e.target.value) === 2?0:null;
-    loadChannelPage(editorPick, minimalReputation, isTag);
-  };
+  const top10Channel=()=>{
+    const dataSource = [
+      {
+        name: '语雀的天空',
+        image:
+          'https://gw.alipayobjects.com/zos/antfincdn/efFD%24IOql2/weixintupian_20170331104822.jpg',
+        desc: '我是一条测试的描述',
+      },
+      {
+        name: 'Ant Design',
+        image:
+          'https://gw.alipayobjects.com/zos/antfincdn/efFD%24IOql2/weixintupian_20170331104822.jpg',
+        desc: '我是一条测试的描述',
+      },
+      {
+        name: '蚂蚁金服体验科技',
+        image:
+          'https://gw.alipayobjects.com/zos/antfincdn/efFD%24IOql2/weixintupian_20170331104822.jpg',
+        desc: '我是一条测试的描述',
+      },
+      {
+        name: 'TechUI',
+        image:
+          'https://gw.alipayobjects.com/zos/antfincdn/efFD%24IOql2/weixintupian_20170331104822.jpg',
+        desc: '我是一条测试的描述',
+      },
+    ];
 
-  const loadChannelPage=(editorPick:number| null,minimalReputation:number,isTag:number|null) => {
-    let params = {
-      pageNum: 1,
-      pageSize: 10,
-      editorPick: editorPick,
-      minimalReputation: minimalReputation,
-      isTag: isTag
-    };
-    dispatch({
-      type: 'trends/getChannelPage',
-      payload: params
-    });
+    return (<div>
+      <ProList<any>
+        toolBarRender={() => {
+          return [
+            <Button key="add" type="primary">
+              新建
+            </Button>,
+          ];
+        }}
+        onRow={(record: any) => {
+          return {
+            onMouseEnter: () => {
+              console.log(record);
+            },
+            onClick: () => {
+              console.log(record);
+            },
+          };
+        }}
+        rowKey="name"
+        headerTitle="文章数量Top10频道"
+        tooltip="基础列表的配置"
+        dataSource={dataSource}
+        showActions="hover"
+        showExtra="hover"
+        metas={{
+          title: {
+            dataIndex: 'name',
+          },
+          avatar: {
+            dataIndex: 'image',
+          },
+          description: {
+            dataIndex: 'desc',
+          },
+          subTitle: {
+            render: () => {
+              return (
+                <Space size={0}>
+                  <Tag color="blue">Ant Design</Tag>
+                  <Tag color="#5BD8A6">TechUI</Tag>
+                </Space>
+              );
+            },
+          },
+          actions: {
+            render: (text, row) => [
+              <a href={row.html_url} target="_blank" rel="noopener noreferrer" key="link">
+                链路
+              </a>,
+              <a href={row.html_url} target="_blank" rel="noopener noreferrer" key="warning">
+                报警
+              </a>,
+              <a href={row.html_url} target="_blank" rel="noopener noreferrer" key="view">
+                查看
+              </a>,
+            ],
+          },
+        }}
+      />
+    </div>);
   };
 
   const option = {
@@ -146,7 +202,14 @@ const TableList: React.FC<ITrendPageProps> = ({ trends, dispatch, loading }) => 
           </Row>
       </Card>
       <Card>
-        <ReactECharts option={option} style={{ height: '700px', width: '100%' }}/>
+        <Row gutter={24}>
+          <Col span={12}>
+            <ReactECharts option={option} style={{ height: '700px', width: '100%' }}/>
+          </Col>
+          <Col span={12}>
+            {top10Channel()}
+          </Col>
+        </Row>
       </Card>
     </div>
   );
